9335850cd0baa4a165057dbb5118dadf978fe070
mrpal39 <rp9545416@gmail.com>
2021-10-30 10:35:18 +0530

    static

--- src/urls.py
+++ src/urls.py
@@ -1,31 +1,31 @@
 
 from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import handler404
 from django.urls.conf import include
-from common.views import home_page,api
+from common.views import home_page, api
 from events.admin import event_admin_site
 
 
 urlpatterns = [
     path('entity-admin/', admin.site.urls),
     path('event-admin/', event_admin_site.urls),
-    path('accounts/',include('accounts.urls')),
-    path('',home_page,name='homepage'),
-    path('pet/',include('pet.urls'),name='homepage'),
-    path("api/",api,name='apiView'),
+    path('accounts/', include('accounts.urls')),
+    path('', home_page, name='homepage'),
+    path('pet/', include('pet.urls'), name='homepage'),
+    path("api/", api, name='apiView'),
     re_path(r'^filer/', include('filer.urls')),
     path('api-auth/', include('rest_framework.urls'))
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
-urlpatterns +=[
+urlpatterns += [
 
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'accounts.views.error_404_view'

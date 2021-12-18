13d353ffdc8042d9f6060a462c47bd2fe02c0768
mrpal39 <rp9545416@gmail.com>
2021-12-18 07:34:50 +0530

    mide fileupdate

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -26,13 +26,13 @@ STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static/staticfiles/'),
 )
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = '../media/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
-STATIC_ROOT = 'static_root/'
+STATIC_ROOT ='static/static_root/'
 
 
 

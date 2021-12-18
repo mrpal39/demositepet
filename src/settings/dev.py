a338dd613247d39010e67dc2d804c9f2f845d03c
mrpal39 <rp9545416@gmail.com>
2021-12-18 08:21:37 +0530

    updateffd

--- src/settings/dev.py
+++ src/settings/dev.py
@@ -26,7 +26,7 @@ STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../staticfiles/'),
+    os.path.join(BASE_DIR, '../../static/staticfiles/'),
 )
 MEDIA_ROOT = '../media/'
 

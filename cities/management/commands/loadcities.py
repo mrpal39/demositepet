9b502298ac6e1f5357c64e001b54663b15f97bf0
mrpal39 <rp9545416@gmail.com>
2021-12-19 09:49:47 +0530

    upda

--- cities/management/commands/loadcities.py
+++ cities/management/commands/loadcities.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         country = "India"
-        utils.load_states_from_file(utils.get_states_filename(country))
+        # utils.load_states_from_file(utils.get_states_filename(country))
         utils.load_cities_from_file(utils.get_cities_filename(country))
         # utils.load_post_cities_from_file(utils.get_postcode_cities_filename(country))
--- cities/utils.py
+++ cities/utils.py
@@ -8,8 +8,8 @@ from django.conf import settings
 from cities import models
 
 
-def get_states_filename(country):
-    return os.path.join(settings.CITIES_DATA_LOCATION, country.lower(), "citie.csv")
+# def get_states_filename(country):
+#     return os.path.join(settings.CITIES_DATA_LOCATION, country.lower(), "citie.csv")
 
 
 def get_cities_filename(country):
@@ -26,28 +26,29 @@ def load_file(filename):
         return list(reader)
 
 
-def load_states_from_file(filename):
-    states = load_file(filename)
+# def load_states_from_file(filename):
+#     states = load_file(filename)
 
-    for state in states:
-        models.State.objects.get_or_create(
-            name=state.get("statename"), abbr=state.get("statename")
-        )
+#     for state in states:
+#         models.State.objects.get_or_create(
+#             name=state.get("statename"), abbr=state.get("statename")
+#         )
+#         print(state.get("statename"))
 
 
-# def load_cities_from_file(filename):
-#     cities = load_file(filename)
-#     cities = sorted(cities, key=lambda c: c["name"])
-#     grouped_cities = itertools.groupby(cities, key=lambda c: c["code"])
-#     # print (states)
+def load_cities_from_file(filename):
+    cities = load_file(filename)
+    cities = sorted(cities, key=lambda c: c["Districtname"])
+    grouped_cities = itertools.groupby(cities, key=lambda c: c["statename"])
+    # print (states)
 
-#     for state_abbr, cities in grouped_cities:
-#         states = models.State.objects.get(code=state_abbr)
-#         print(states)
-        # for city in cities:
-            # models.District.objects.get_or_create(
-        #         state=states, name=city.get("name"),code=city.get("code"),
-        #     )
+    for state_abbr, cities in grouped_cities:
+        states = models.State.objects.get(abbr=state_abbr)
+        print(states)
+        for city in cities:
+            models.District.objects.get_or_create(
+                state=states, name=city.get("Districtname"),code=city.get("pincode"),
+            )
 
 
         #     print(city.get("name"))
